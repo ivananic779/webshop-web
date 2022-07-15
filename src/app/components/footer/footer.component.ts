c791d4ae540ab52ae7160425ddf2903c365bd844
Ivan Anic <i.anic779@gmail.com>
2022-07-15 23:28:08 +0200

    small fix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,6 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  public environmentName = environment.name;
 
   test: Date = new Date();
 
